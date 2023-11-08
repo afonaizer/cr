bf8ad076f183eebfb5cb5023ced4be503e9dd5ea
afonaizer <123896092+afonaizer@users.noreply.github.com>
2023-11-08 19:40:24 +0200

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+       
+        static void Sqr()
+        {
+            Console.Write("введ число для возвед в квадрат ");
+            double x = double.Parse(Console.ReadLine());
+            double y = Math.Pow(x, 2);
+            Console.WriteLine($"число {x} в квадрате {y}");
+        }
+        static void Main(string[] args)
+        {
+            Sqr();
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
